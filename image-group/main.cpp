1cb7714a3bcf7ed9be86d78cf3e5c3fd470cc89f
don-reba <don_reba@inbox.ru>
2012-06-08 05:22:35 +0200

    Added JSON processing.

--- image-group/main.cpp
+++ image-group/main.cpp
@@ -1,11 +1,37 @@
 #include "gist.hpp"
 
+#include "json/json.h"
+
+#include <stdlib.h>
+
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char * argv[])
 {
+	Json::Value root;
+	Json::Reader reader;
+
+	ifstream stream("venice.json");
+	if (!reader.parse(stream, root, false))
+	{
+		cout << "Input JSON data could not be parsed.\n";
+		cout << reader.getFormattedErrorMessages();
+		return EXIT_FAILURE;
+	}
+
+	Json::Value castingCall(root["response"]["castingCall"]);
+
+	Json::Value photos(castingCall["CastingCall"]["Auditions"]["Audition"]);
+
+	for (int i(0), size(photos.size()); i != size; ++i)
+	{
+		cout << photos[i]["id"].asString() << "\n";
+	}
+
+	/*
 	Descriptor descriptor;
 
 	int blockCount = 4;
@@ -17,4 +43,7 @@ int main(int argc, char * argv[])
 	ofstream stream("out.txt");
 	for (size_t i(0), size(descriptor.size()); i != size; ++i)
 		stream << descriptor[i] << '\n';
+	*/
+
+	return EXIT_SUCCESS;
 }
