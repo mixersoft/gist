91eb7d75837133dd43b07af9459b5aa3c3a8b1cb
don-reba <don_reba@inbox.ru>
2012-07-11 11:13:16 +0200

    Initial unit test setup.

---
+++ production/image-group-test/TestCluster.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE image-group test
+
+#include "Cluster.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+using namespace boost;
+
+BOOST_AUTO_TEST_CASE(UnorderedClustering)
+{
+}
