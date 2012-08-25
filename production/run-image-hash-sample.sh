75997a658376519e574209306207129ed008c9f2
mixersoft <michael@snaphappi.com>
2012-08-25 11:55:57 -0400

    add auto-rotate using exiftran before calculating hash uses sudo apt-get install exiftran

--- production/run-image-hash-sample.sh
+++ production/run-image-hash-sample.sh
@@ -10,8 +10,14 @@ cat /dev/null > $output
 # store a list of hashes in the output file
 for image in $sample/images/*
 do
-	hash=$(bin/image-hash $image)
+	rotate=$(exiftran -d $image  2>/dev/null | grep Orientation | grep -i --count 'bottom\|right')
+	if [ "$rotate" != "0" ]; then
+		/usr/bin/exiftran -a $image -o /var/tmp/auto-rotated.tmp
+		hash=$(bin/image-hash /var/tmp/auto-rotated.tmp)
+	else 
+		hash=$(bin/image-hash $image)
+	fi
 	filename=$(basename $image)
 	id=${filename%.*}
-	echo $id\|$hash >> $output
+	echo $id\|$hash\|$rotate >> $output
 done
