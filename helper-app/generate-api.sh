dbd3d82e8c3c790de03ad27d9dab7b4478cbb5e8
don-reba <don_reba@inbox.ru>
2012-09-17 22:41:28 +0200

    Added a Thrift API generation script and an API placeholder.

---
+++ helper-app/generate-api.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd api
+
+# delete any vestiges of the old API
+
+for dir in php-server js-client cs-client
+do
+	rm -f -r $dir
+	mkdir $dir
+done
+
+# generate new API
+
+src=tutorial.thrift
+
+thrift --gen php:server -out php-server $src
+thrift --gen csharp -out cs-client $src
+thrift --gen js:jquery -out js-client $src
