f9273955f55f88d248a784d4e5a8e5627bbf596e
don-reba <don_reba@inbox.ru>
2012-11-02 11:12:10 +0100

    Renamed the image-grouping project to "pipeline".

--- research/test.cmd
+++ research/test.cmd
@@ -1,3 +1,3 @@
 @echo off
 set nunit="C:\Program Files (x86)\NUnit 2.6\bin\nunit-console.exe"
-%nunit%  /nologo /noresult /framework=4.0 bin\ImageGroupingTest.dll
+%nunit%  /nologo /noresult /framework=4.0 bin\PipelineTest.dll
