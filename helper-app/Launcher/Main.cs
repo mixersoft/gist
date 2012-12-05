9af5853e2f739ba5a2f07eb3545955792cc9a9c8
don-reba <don_reba@inbox.ru>
2012-12-05 04:04:44 +0100

    Added TopLevelFolder to the installer bootstrapper. The Launcher app is used to install the AIR file using Adobe's arh.exe.

---
+++ helper-app/Launcher/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Launcher
+{
+	class Launcher
+	{
+		static int Main(string[] args)
+		{
+			Environment.CurrentDirectory = AssemblyDirectory;
+			var process = CreateProcess(args);
+			if (process == null)
+				return 0;
+			process.WaitForExit();
+			return process.ExitCode;
+		}
+
+		static Process CreateProcess(string[] args)
+		{
+			var info = new ProcessStartInfo();
+			info.CreateNoWindow  = true;
+			info.UseShellExecute = false;
+			switch (args.Length)
+			{
+				case 1:
+					info.FileName = args[0];
+					return Process.Start(info);
+				case 2:
+					info.FileName  = args[0];
+					info.Arguments = args[1];
+					return Process.Start(info);
+				default:
+					return null;
+			}
+		}
+
+		static string AssemblyDirectory
+		{
+			get { return Path.GetDirectoryName(typeof(Launcher).Assembly.Location); }
+		}
+	}
+}
