a6fbdfc355f0a86aeb536c8f8fc99e0c13680a24
don-reba <don_reba@inbox.ru>
2013-02-15 19:55:18 +0100

    Added a file that was supposed to be committed earlier.

---
+++ helper-app/client/src/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Snaphappi
+{
+	public class Logger
+	{
+		public void RecordException(Exception e)
+		{
+			using (var writer = File.AppendText(FilePath))
+			{
+				writer.WriteLine(DateTime.Now);
+				writer.WriteLine(e);
+				writer.WriteLine();
+			}
+		}
+
+		private string FilePath
+		{
+			get
+			{
+				var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+				var dir = Path.Combine(appData, "Snaphappi");
+				Directory.CreateDirectory(dir);
+				return Path.Combine(dir, "log.txt");
+			}
+		}
+	}
+}
